195ee117544509deed27d58b9afaae6ad4286fb2
Andrey <shock.work@mail.ru>
2021-02-16 22:58:38 +0300

    remove excess logic in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('The sedulous hyena ate the antelope!');
-});
-app.listen(port, () => {
-    console.log(`server is listening on ${port}`);
+
+app.use(express.json())
+
+app.listen(port, (): void => {
+    console.log(`Server has been started at ${port} port`);
 });
