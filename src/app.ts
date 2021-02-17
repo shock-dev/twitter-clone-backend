4fe52dc761576c72e7b3588e3b48f35665a327b7
Andrey <shock.work@mail.ru>
2021-02-17 13:47:46 +0300

    connecting to mongo in main file - app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import userRoutes from "./routes/users";
+import connectDb from "./core/db";
 
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
+app.use("/users", userRoutes);
 
-app.listen(port, (): void => {
-    console.log(`Server has been started at ${port} port`);
-});
+const start = async (): Promise<void> => {
+    try {
+        await connectDb();
+        app.listen(port, (): void => {
+            console.log(`Server has been started at ${port} port`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
