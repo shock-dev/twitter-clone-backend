589445bd719595205f4e73fa99fbcfe8c7f869a8
Andrey <shock.work@mail.ru>
2021-02-18 18:05:55 +0300

    registration user in controller realized

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,6 @@
 import User from "../models/User";
+import { validationResult  } from "express-validator";
+import generateMD5 from "../utils/generateHash";
 import express from "express";
 
 class UserController {
@@ -6,6 +8,32 @@ class UserController {
         try {
             const users: any[] = await User.find();
             res.json(users);
+        } catch (e) {
+            res.status(400).json(e);
+        }
+    }
+    async register(req: express.Request, res: express.Response): Promise<void> {
+        try {
+            // Checking for errors in fields
+            const errors: any = validationResult(req);
+            if (!errors.isEmpty()) {
+                res.status(400).json({ errors: errors.array() });
+                return;
+            }
+            // Prepare data to register
+            const { email, username, fullname, password } = req.body;
+            const stringForHash: string = process.env.SECRET_KEY || Math.random().toString();
+            const formData: object = {
+                email,
+                username,
+                fullname,
+                password,
+                confirmed_hash: generateMD5(stringForHash)
+            }
+            // Create user
+            const user: any = new User(formData);
+            await user.save();
+            res.json(user);
         } catch (e) {
             res.status(400).json(e);
         }
