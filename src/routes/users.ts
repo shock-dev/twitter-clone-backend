cd291e5fa070a29eb664f6a396d4c47cc43f7222
Andrey <shock.work@mail.ru>
2021-02-18 15:35:57 +0300

    new route for registration

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import authValidator from "../validations/auth.validation";
 import controller from "../controllers/user.controller";
 
 const router = Router();
 
 router.get("/", controller.getAll);
+router.post("/register", authValidator, controller.register);
 
 export default router;
