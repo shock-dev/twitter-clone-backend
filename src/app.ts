ff72ed8afa4005cb7444d8e761a5fc79d91e26db
Andrey <shock.work@mail.ru>
2021-02-18 20:43:22 +0300

    fix dotenv in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import userRoutes from "./routes/users";
 import connectDb from "./core/db";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/users", userRoutes);
