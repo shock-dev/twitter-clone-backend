d7d0a7ef7a2b1030bb04df5df1e7a5d23c7120b2
Andrey <shock.work@mail.ru>
2021-02-18 14:46:12 +0300

    more model fields added

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,15 @@
 import { model, Schema } from "mongoose";
 
 const userSchema = new Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+    email: { type: String, required: true, unique: true },
+    fullname: { type: String, required: true },
+    username: { type: String, required: true },
+    location: { type: String },
+    password: { type: String, required: true },
+    confirmed: { type: Boolean, required: true },
+    confirmed_hash: { type: String, required: true },
+    about: { type: String },
+    website: { type: String }
 })
 
 export default model("users", userSchema);
