e0dce504bc4128b0f2d27e1ceeed032c25f222e3
Andrey <shock.work@mail.ru>
2021-02-18 20:51:56 +0300

    default callback function after sending email realized

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,4 +1,5 @@
 import mailer from "../core/mailer";
+import { SentMessageInfo } from "nodemailer";
 
 interface SendEmailProps {
     from: string
@@ -7,6 +8,14 @@ interface SendEmailProps {
     html: string
 }
 
-export default async ({ from, to, subject, html }: SendEmailProps, callback): Promise<void> => {
+const defaultCallback = (error: Error | null, info: SentMessageInfo): void => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(info)
+    }
+}
+
+export default async ({ from, to, subject, html }: SendEmailProps, callback = defaultCallback): Promise<void> => {
     await mailer.sendMail({ from, to, subject, html }, callback);
 }
