b5951ca8c540245335c3fa3f194efc9b0a5601a3
Andrey <shock.work@mail.ru>
2021-02-18 22:29:35 +0300

    method `show` in controller realized

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -67,6 +67,15 @@ class UserController {
             res.status(400).json(e);
         }
     }
+    async show(req: express.Request, res: express.Response): Promise<void> {
+        try {
+            const { id } = req.params;
+            const user = await User.findById(id);
+            res.json(user);
+        } catch (e) {
+            res.status(404).json({ message: "Пользователь не найден!" });
+        }
+    }
 }
 
 export default new UserController();
