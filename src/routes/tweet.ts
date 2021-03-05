661c93a11f4344dad5adb6c4bb90038ad37ff2f7
Andrey <shock.work@mail.ru>
2021-03-05 14:24:23 +0300

    routes for tweets

---
+++ src/routes/tweet.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import controller from "../controllers/tweet.controller";
+import passport from '../core/passport';
+
+const router = Router();
+
+router.get('/', controller.getAll);
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create);
+router.get('/:id', controller.show);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.delete);
+
+export default router;
