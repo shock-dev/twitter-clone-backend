d5ab53b2f7d01472a03723b649dbfd39ea0e7f02
Andrey <shock.work@mail.ru>
2021-03-05 10:55:28 +0300

    model for tweets

---
+++ src/models/Tweet.ts
@@ -0,0 +1,8 @@
+import { model, Schema } from "mongoose";
+
+const tweetSchema = new Schema({
+    text: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'users', required: true }
+})
+
+export default model("tweets", tweetSchema);
