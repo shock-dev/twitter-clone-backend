27f1c2fa7a6b6b46ff59b82f93565b6b5522ca70
Andrey <shock.work@mail.ru>
2021-03-23 20:33:41 +0300

    configuration of routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header';
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Redirect
 } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import NoMatch from './pages/NoMatch';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserInfo } from './store/actions/user';
 
 const App = () => {
+    const dispatch = useDispatch();
+    const {info} = useSelector(({user}) => user);
+
+    useEffect(() => {
+        dispatch(fetchUserInfo());
+    }, []);
+
     return (
         <Router>
-          <Header />
-          <div className="page container">
-              <Switch>
-                  <Route path="/" exact>
-                      <Home />
-                  </Route>
-                  <Route path="/login" exact>
-                      <Login />
-                  </Route>
-                  <Route path="/register" exact>
-                      <Register />
-                  </Route>
-                  <Route path="*">
-                      <NoMatch />
-                  </Route>
-              </Switch>
-          </div>
+            <Header />
+            <div className="page container">
+                {info !== null ? (
+                    <Switch>
+                        <Route path="/" exact>
+                            <Home />
+                        </Route>
+                        <Redirect to="/" />
+                    </Switch>
+                ) : (
+                    <Switch>
+                        <Route path="/login" exact>
+                            <Login />
+                        </Route>
+                        <Route path="/register" exact>
+                            <Register />
+                        </Route>
+                        <Redirect to="/login" />
+                    </Switch>
+                )}
+            </div>
         </Router>
     );
 };
