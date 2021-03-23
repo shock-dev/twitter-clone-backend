b9e38bbb7cfeef4e390cf9dafea3f079b9e02824
Andrey <shock.work@mail.ru>
2021-03-23 19:09:19 +0300

    use interceptor in axios for requests

---
+++ client/src/core/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+axios.interceptors.request.use((config) => {
+    config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default axios;
