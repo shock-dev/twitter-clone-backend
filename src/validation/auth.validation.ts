062c34d5571a0efc3a14e62ea0b45b5f6f9e2a18
Andrey <shock.work@mail.ru>
2021-04-18 09:49:50 +0300

    improve code in auth validation

--- src/validation/auth.validation.ts
+++ src/validation/auth.validation.ts
@@ -1,32 +1,42 @@
 import { body } from 'express-validator';
 
+const emailLength = {
+  min: 10,
+  max: 40
+};
+
+const fullnameLength = {
+  min: 2,
+  max: 40
+};
+
+const usernameLength = {
+  min: 2,
+  max: 40
+};
+
+const passwordLength = {
+  min: 6
+};
+
 export default [
   body('email', 'Введите email')
     .isEmail()
     .withMessage('Введите ваш email корректно')
-    .isLength({
-      min: 10,
-      max: 40
-    })
-    .withMessage('Допустимое кол-во слов email от 10 до 40'),
+    .isLength(emailLength)
+    .withMessage(`Допустимое кол-во слов email от ${emailLength.min} до ${emailLength.max}`),
   body('fullname', 'Введите fullname')
     .isString()
-    .isLength({
-      min: 2,
-      max: 40
-    })
-    .withMessage('Допустимое кол-во слов fullname от 2 до 40'),
+    .isLength(fullnameLength)
+    .withMessage(`Допустимое кол-во слов fullname от ${fullnameLength.min} до ${fullnameLength.max}`),
   body('username', 'Введите username')
     .isString()
-    .isLength({
-      min: 2,
-      max: 40
-    })
-    .withMessage('Допустимое кол-во слов username от 2 до 40'),
+    .isLength(usernameLength)
+    .withMessage(`Допустимое кол-во слов username от ${usernameLength.min} до ${usernameLength.max}`),
   body('password', 'Введите password')
     .isString()
-    .isLength({ min: 2 })
-    .withMessage('Допустимое кол-во слов password от 6')
+    .isLength(passwordLength)
+    .withMessage(`Допустимое кол-во слов password от ${passwordLength.min}`)
     .custom((value, { req }) => {
       if (value !== req.body.password_confirm) {
         throw new Error('Пароли не совпадают');
