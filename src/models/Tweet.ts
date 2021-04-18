f3af4bb236b76672cdcd954ef481052de56b5337
Andrey <shock.work@mail.ru>
2021-04-18 19:29:18 +0300

    add-on for the model

--- src/models/Tweet.ts
+++ src/models/Tweet.ts
@@ -3,13 +3,16 @@ import { model, Schema } from 'mongoose';
 const tweetSchema = new Schema({
   text: {
     type: String,
-    required: true
+    required: true,
+    maxlength: 280
   },
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     required: true
   }
+}, {
+  timestamps: true
 });
 
 export default model('Tweet', tweetSchema);
