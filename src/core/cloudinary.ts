6d206e3094eb7e1b6cd7959a75a1f29f8d21268e
Andrey <shock.work@mail.ru>
2021-04-27 15:10:54 +0300

    config file of cloudinary

---
+++ src/core/cloudinary.ts
@@ -0,0 +1,13 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+if (!process.env.CLOUDINARY_NAME) {
+  throw new Error('Отсутствует конфигурация для Cloudinary');
+}
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+export default cloudinary;
